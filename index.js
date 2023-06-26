c21d33de3118a86df746bf908f779b93ba0d87a6
Morinke01 <ejulius250@gmail.com>
2023-06-26 05:09:36 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+//removing main node
+document.getElementById('main').remove()
+
+//creating a new element
+let newHeader = document.createElement('h1')
+    newHeader.id = 'victory'
+
+//creating newHeader as the variable with a message
+newHeader.innerHTML = "Morinke is the champion";
